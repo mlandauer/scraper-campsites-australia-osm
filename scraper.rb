83623056103846afe26df644144730c82db3c506
Matthew Landauer <matthew@today.design>
2017-09-28 16:14:32 +1000

    Example overpass api query

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,17 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
+require 'scraperwiki'
+require 'mechanize'
+
+agent = Mechanize.new
+
+# Example overpass API query that gets campsites within a part of Sydney
+# from OpenStreetMap
+page = agent.get('https://overpass-api.de/api/interpreter?data=node%5B%22tourism%22%3D%22camp%5Fsite%22%5D%28%2D34%2E011688599109%2C150%2E96038818359%2C%2D33%2E652923027488%2C151%2E44172668457%29%3Bout%3Bway%5B%22tourism%22%3D%22camp%5Fsite%22%5D%28%2D34%2E011688599109%2C150%2E96038818359%2C%2D33%2E652923027488%2C151%2E44172668457%29%3B%28%2E%5F%3B%3E%3B%29%3Bout%3B')
+
+puts page.body
+
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
 #
